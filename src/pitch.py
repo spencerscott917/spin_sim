6c39874151d12277af38b98ec6d16b0bc0c3b63b
Spencer Scott <Spencer@Spencers-MacBook-Pro-5.local>
2018-08-16 18:44:28 -0400

    formatting code

--- src/pitch.py
+++ src/pitch.py
@@ -1,5 +1,10 @@
 """My implementation of the complicated physics of ball flight. Built
-from scratch, refs for physics to be added in writeups,"""
+from scratch, refs for physics to be added in writeups"""
+
+# TODO: - euler's method
+#       - see what Pitch f/x data are available to be able to use in
+#         simulation. GOAL: use only the data currently provided.
+#       - look at using Pitch f/x data as a calibration tool
 
 
 class Pitch(object):
@@ -11,10 +16,14 @@ class Pitch(object):
         x is positive from pitcher to catcher,
         y is positive to the pitcher's right,
         z is positive up."""
+
         # Velocity out of the pitcher's hand
+        # TODO: split this into component vectors
         self.v0 = v0
-        # spin in the xy plane, clockwise from top-down is positive.
+
+        # spin in the xy plane, clockwise from top-down view is positive.
         self.xy_spin = xy_spin
+
         # initialize pitch release as 0,0,0 (Change?)
         self.x = [0]
         self.y = [0]
@@ -28,9 +37,9 @@ class Pitch(object):
         # z0 = self.z[-1]
 
         # need to account for drag, doing simplest implementation first
-        # need to solve gravity equation properly, currently the ball falls
-        # further and further as time resolution increases. SOLN: break v0
-        # into component vectors
+        # SOLN: break v0 into component vectors
+        # implement euler's method for Diff Eq solver (or something
+        # more computationally efficient)
         x1 = self.x[0] + self.v0*t
         y1 = self.y[0]
         z1 = self.z[0] - 0.5*9.81*t**2
@@ -42,12 +51,15 @@ class Pitch(object):
     def throw(self, dt):
         """the main simulation portion of the code"""
         t = 0
+
         # 18.44 is distance from mound to plate in meters
         while self.x[-1] <= 18.44:
             self._update_pos(t, dt)
             t += dt
+
         # make trajectory a copy of the lists so reset doesn't erase results
         trajectory = [self.x[:], self.y[:], self.z[:]]
+
         # reset coordinates for new run
         self.x = [0]
         self.y = [0]
